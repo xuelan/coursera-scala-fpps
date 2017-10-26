3352c156c483a8cd5c357669f76910ce56af7398
xuelan SUN <xuelan.sun@gmail.com>
2017-10-26 12:14:39 +0200

    Add filtering

--- week3/objsets/src/main/scala/objsets/TweetSet.scala
+++ week3/objsets/src/main/scala/objsets/TweetSet.scala
@@ -41,7 +41,7 @@ abstract class TweetSet {
    * Question: Can we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def filter(p: Tweet => Boolean): TweetSet = ???
+  def filter(p: Tweet => Boolean): TweetSet = filterAcc(p, new Empty())
   
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
@@ -54,7 +54,7 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def union(that: TweetSet): TweetSet = ???
+  def union(that: TweetSet): TweetSet = ???
   
   /**
    * Returns the tweet from this set which has the greatest retweet count.
@@ -107,7 +107,8 @@ abstract class TweetSet {
 }
 
 class Empty extends TweetSet {
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
   
   /**
    * The following methods are already implemented
@@ -124,7 +125,13 @@ class Empty extends TweetSet {
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = {
+    if(p(elem)){
+      right.filterAcc(p, left.filterAcc(p, acc.incl(elem)))
+    } else {
+      right.filterAcc(p, left.filterAcc(p, acc))
+    }
+  }
   
     
   /**
@@ -177,17 +184,17 @@ class Cons(val head: Tweet, val tail: TweetList) extends TweetList {
 
 
 object GoogleVsApple {
-  val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
-  val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
+    val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
+    val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
     lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
-  
-  /**
-   * A list of all tweets mentioning a keyword from either apple or google,
-   * sorted by the number of retweets.
-   */
-     lazy val trending: TweetList = ???
+    lazy val appleTweets: TweetSet = ???
+
+    /**
+     * A list of all tweets mentioning a keyword from either apple or google,
+     * sorted by the number of retweets.
+     */
+    lazy val trending: TweetList = ???
   }
 
 object Main extends App {
