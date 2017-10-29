bcf8826754364928ff24877b956c5d475c832a09
xuelan SUN <xuelan.sun@gmail.com>
2017-10-29 22:44:01 +0100

    Add exo in week3

--- week3/objsets/src/main/scala/course/List.scala
+++ week3/objsets/src/main/scala/course/List.scala
@@ -17,3 +17,13 @@ class Nil[T] extends List[T]{
   def tail: Nothing = throw new NoSuchElementException("Nil.tail")
 }
 
+//Exercices in Week4 Video2
+object List{
+  def apply[T](x1: T, x2: T):List[T] = new Cons(x1, new Cons(x2, new Nil))
+
+  def apply[T](x1: T): List[T] = new Cons(x1, new Nil)
+
+  def apply[T]: List[T] = new Nil
+
+}
+
